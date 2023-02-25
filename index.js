8d6bc86e4fcd5333f8f6426e97fe8caba68c2ffa
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-02-25 01:36:14 -0500

    implement main div

--- index.js
+++ index.js
@@ -5,13 +5,43 @@ $(()=>{
 
     const logoElement = $("#logoTexts");
     const logoFontSizeVal = parseInt($(logoElement).css('font-size').replace(/px/,""))
-    const logoFontSizeMinLimit = 30;
+    const logoStartTopVal = parseInt($(logoElement).css('top').replace(/px/,""));
+    console.log(logoStartTopVal);
+    const logoScrollValMinLimit = -6;
+    const logoFontSizeMinLimit = 10;
+
+    const titleContainerElem = $("#titleContainer");
+    const titleContainerStartTopVal = parseInt($(titleContainerElem).css('top').replace(/px/,""));
+    // console.log(titleContainerStartTopVal);
 
     $.event.add(window, "scroll", () => {
         const scrollYVal = window.scrollY;
+
         $(headerElement).css('top', scrollYVal < headerScrollValMaxLimit ? - scrollYVal * headerScrollMagnifier : - headerScrollValMaxLimit);
 
         const targetLogoFontSize = logoFontSizeVal - scrollYVal * 0.08;
         $(logoElement).css('font-size', targetLogoFontSize > logoFontSizeMinLimit ? targetLogoFontSize + "px" : logoFontSizeMinLimit + "px");
+        const targetLogoTopVal = logoStartTopVal - scrollYVal * 5;
+        $(logoElement).css('top', targetLogoTopVal + "px");
+
+        $(titleContainerElem).css('top', (titleContainerStartTopVal-scrollYVal * 1.75) + "px")
     })
+
+    // typed.js
+    var options = {
+        strings: ["<h1>Welcome!</h1>", "<h1 id='typed-strings'><span> I'm </span><abbr title='George Yihao Xu'>George</abbr></h1>"],
+        typeSpeed: 90,
+        backSpeed: 10,
+        backDelay: 2000,
+        loop: true,
+        showCursor: false,
+        onBegin: () => {
+            $('#typed').removeClass("toBlue");
+        },
+        onComplete: () => {
+            $('#typed').addClass("toBlue");
+        },
+      };
+    var typed = new Typed('#typed', options);
 })
+
