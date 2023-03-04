a2816d4ce0cf6eda844c390e534d3f47f19a19fd
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-03-04 15:13:05 -0500

    update osb2.html with floating image box

---
+++ singleWorkPages/ecohome2Bahavior.js
@@ -0,0 +1,7 @@
+import {linkImgDiv} from "../indexVideoManager.js";
+
+$(() => {
+    // linkImgDiv("#osbHiFiImg1","#hifi1Container");
+    // linkImgDiv("#osbWFImg1","#wf1Container");
+    linkImgDiv("#ecohomeHiFiImg3","#ecohomeHiFiImg3Container");
+})
