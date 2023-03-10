f3107f2cf5445bef7b01e3abcd252b7249bcb2af
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-03-09 20:06:30 -0500

    adding more stuff

--- singleWorkPages/osbBahavior.js
+++ singleWorkPages/osbBahavior.js
@@ -3,7 +3,7 @@ import { reveal } from "../reveal.js";
 
 $(() => {
     linkImgDiv("#osbHiFiImg1","#hifi1Container");
-    linkImgDiv("#osbWFImg1","#wf1Container");
+    linkImgDiv("#osbWFImgM","#wf1Container");
 
     // loading animation
     window.addEventListener("scroll", reveal);
