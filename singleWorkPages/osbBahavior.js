87c70aa490076775ab35ce5efa2afad18bf21a8e
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-03-14 19:18:52 -0400

    add

--- singleWorkPages/osbBahavior.js
+++ singleWorkPages/osbBahavior.js
@@ -2,8 +2,8 @@ import {linkImgDiv} from "../indexVideoManager.js";
 import { reveal } from "../reveal.js";
 
 $(() => {
-    linkImgDiv("#osbHiFiImg1","#hifi1Container");
-    linkImgDiv("#osbWFImgM","#wf1Container");
+    // linkImgDiv("#osbHiFiImg1","#hifi1Container");
+    // linkImgDiv("#osbWFImgM","#wf1Container");
 
     // loading animation
     window.addEventListener("scroll", reveal);
